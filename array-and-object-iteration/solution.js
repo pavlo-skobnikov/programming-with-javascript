b1a266fdd720c49512ffad2825435323c6fee19f
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2024-09-09 11:08:00 +0300

    Finish the `Arrays and object iteration` assignment

--- array-and-object-iteration/solution.js
+++ array-and-object-iteration/solution.js
@@ -1,6 +1,42 @@
 // Task 1
+const dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
+
+function logDairy() {
+    for (const item of dairy) {
+        console.log(item)
+    }
+}
+
+logDairy()
 
 // Task 2
+const animal = {
+
+    canJump: true
+
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+
+function birdCan() {
+    for (const property in bird) {
+        if (bird.hasOwnProperty(property)) {
+            console.log(`${property}: ${bird[property]}`)
+        }
+    }
+}
+
+birdCan()
 
 // Task 3
+function animalCan() {
+    for (const property in bird) {
+        console.log(`${property}: ${bird[property]}`)
+    }
+}
 
+animalCan()
